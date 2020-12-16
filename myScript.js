6e3e3a8c8694ede5f34d0f44b2e9cfcea20aa969
Samuel Gerhartinger <samuel.g.anmeldung@gmail.com>
2020-12-16 07:56:54 +0100

    updates

--- myScript.js
+++ myScript.js
@@ -1,19 +1,77 @@
 let left = document.getElementsByClassName('leftArrow');
 let right = document.getElementsByClassName('rightArrow');
 let inhalt = document.getElementsByClassName('product');
-let count = 0;
-let oldCount;
-console.log(inhalt);
-    var i;
-  for (i = 0; i < inhalt.length; i++) {
-    if(inhalt[i].style.display != 'none'){
-        count = i;
-        console.log(count);
-    }
+let count;
+
+var i;
+for (i = 0; i < inhalt.length; i++) {
+  if(inhalt[i].style.display != 'none'){
+      count = i;
   }
+}
+
+let oldCount;
+
+let white = document.getElementsByClassName('white');
+let black = document.getElementsByClassName('black');
+let creme = document.getElementsByClassName('creme');
+let whiteRad = document.getElementsByClassName('whiteRad');
+let blackRad = document.getElementsByClassName('blackRad');
+let cremeRad = document.getElementsByClassName('cremeRad');
+let currentCol = 0;
+
+let frontPicBut = document.getElementsByClassName('frontPicBut');
+let backPicBut = document.getElementsByClassName('backPicBut');
+let closePicBut = document.getElementsByClassName('closePicBut');
+
+let frontPic = document.getElementsByClassName('frontPic');
+let backPic = document.getElementsByClassName('backPic');
+let closePic = document.getElementsByClassName('closePic');
+let currentColorDesign;
+
+if (currentCol == 0) {
+  var i;
+  for (i = 0; i < white.length; i++) {
+    white[i].style.display = 'none';
+    creme[i].style.display = 'none';
+  }
+}
+
+
+pictureSwap();
+
+whiteRad[count].onclick = function() {
+  pictureSwap();
+  var i;
+  for (i = 0; i < white.length; i++) {
+    white[i].style.display = 'block';
+    creme[i].style.display = 'none';
+    black[i].style.display = 'none';
+  }
+}
+blackRad[count].onclick = function() {
+  pictureSwap();
+  var i;
+  for (i = 0; i < white.length; i++) {
+    white[i].style.display = 'none';
+    creme[i].style.display = 'none';
+    black[i].style.display = 'block';
+  }
+}
+cremeRad[count].onclick = function() {
+  pictureSwap();
+  var i;
+  for (i = 0; i < white.length; i++) {
+    white[i].style.display = 'none';
+    creme[i].style.display = 'block';
+    black[i].style.display = 'none';
+  }
+}
+
 
 left[0].addEventListener("click", lolLeft);
 function lolLeft(){
+  currentCol = 0;
     oldCount = count;
     count--;
     if(count<0){count = inhalt.length-1}
@@ -29,28 +87,77 @@ if(count>inhalt.length-1){count = 0}
 }
 
 function lol(direct){
-    console.log("count"+count);
-    console.log("old"+oldCount);
+    var i;
+    for (i = 0; i < white.length; i++) {
+      white[i].style.display = 'none';
+      creme[i].style.display = 'none';
+      black[i].style.display = 'block';
+    }
+    var i;
+    for (i = 0; i < white.length; i++) {
+      white[i].style.display = 'none';
+      creme[i].style.display = 'none';
+      black[i].style.display = 'block';
+    }
     inhalt[oldCount].style.display = 'none';
     inhalt[count].style.display = 'flex';
- /* if(direct = 'r'){
-    inhalt[count].animate([
-  // keyframes
-  { transform: 'translateX(20px)' }
-], { 
-  // timing options
-  duration: 1000
-});
+    whiteRad[count].onclick = function() {
+      pictureSwap();
+      var i;
+      for (i = 0; i < white.length; i++) {
+        white[i].style.display = 'block';
+        creme[i].style.display = 'none';
+        black[i].style.display = 'none';
+      }
+    }
+    blackRad[count].onclick = function() {
+      pictureSwap();
+      var i;
+      for (i = 0; i < white.length; i++) {
+        white[i].style.display = 'none';
+        creme[i].style.display = 'none';
+        black[i].style.display = 'block';
+      }
+    }
+    cremeRad[count].onclick = function() {
+      pictureSwap();
+      var i;
+      for (i = 0; i < white.length; i++) {
+        white[i].style.display = 'none';
+        creme[i].style.display = 'block';
+        black[i].style.display = 'none';
+      }
+    }
+    
+}
+
+
+function pictureSwap() {
+  let i;
+  for (i = 0; i < frontPicBut.length; i++) {
+    if(frontPicBut[i].style.display != 'none'){
+      currentColorDesign = i;
+    }
   }
-  setTimeout(function() 
-{ 
-  inhalt[count].style.display = 'none';
-  //inhalt[count+1].style.display = 'block'
-}, 
-3000);
-  var i;
-  //for (i = 0; i < inhalt.length; i++) {
-    //inhalt[i].style.display='none';
-    //inhalt[count].style.display='block';
-  //}*/
+  console.log(currentColorDesign);
+  console.log('sex');
+    frontPic[currentColorDesign].style.display = 'block';
+    backPic[currentColorDesign].style.display = 'none';
+    closePic[currentColorDesign].style.display = 'none';
+
+  frontPicBut[currentColorDesign].onclick = function(){
+    frontPic[currentColorDesign].style.display = 'block';
+    backPic[currentColorDesign].style.display = 'none';
+    closePic[currentColorDesign].style.display = 'none';
+  }
+  backPicBut[currentColorDesign].onclick = function(){
+    frontPic[currentColorDesign].style.display = 'none';
+    backPic[currentColorDesign].style.display = 'block';
+    closePic[currentColorDesign].style.display = 'none';
+  }
+  closePicBut[currentColorDesign].onclick = function(){
+    frontPic[currentColorDesign].style.display = 'none';
+    backPic[currentColorDesign].style.display = 'none';
+    closePic[currentColorDesign].style.display = 'block';
+  }
 }
--- productDetailed.php
+++ productDetailed.php
@@ -11,17 +11,21 @@
     <link rel="stylesheet" href="stylesheetHeader.css">
     <link rel="stylesheet" href="stylesheetProductDetailed.css">
     <link rel="stylesheet" href="https://use.typekit.net/bbg6boj.css">
-    <link rel="stylesheet" href="https://use.typekit.net/owz8zel.css">
+    <link rel="shortcut icon" href="pictures/logo_FINAL.svg"/>
     <title>No Heart</title>
     <script language="javascript" type="text/javascript" src="myScript.js" defer></script>
 </head>
 <body>
-    <header class="container ">
+    <header class="container headerDesktop">
+        <div class="row headerBackground"></div>
         <div class="row logoLine">
-            <div class="col-1 black-stripe"></div>
-            <div class="logo col-2 col-lg-2 col-md-3"><img src="pictures/logo_blau_2.png"></div>
-            <div class="col-9 col-lg-9 col-md-8 black-stripe"></div>
+            <div class="col-2 black-stripe"></div>
+            <div class="logo col-2 col-lg-2 col-md-3">
+                <img src="pictures/logo_FINAL.svg">
+            </div>
+            <div class="col-9 col-lg-8 col-md-7 black-stripe"></div>
         </div>
+        
         <div class="row navBarLine2">
             <div class="col-7">
             </div>
@@ -36,67 +40,226 @@
             </nav>
         </div>
     </header>
+    <header class="container headerMobile">
+        <div class="row headerRowM">
+            <div class="col-3 logoM"><img src="pictures/logo_FINAL.svg" class="logoMImg"></div>
+            <div class="col-6 spaceM"></div>
+            <div class="col-3 burgerM">
+                <svg stroke="#B5B5B5" width="55" height="55" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" fill="none"><path xmlns="http://www.w3.org/2000/svg" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16"></path></svg>
+            </div>
+        </div>
+    </header>
+
+    
 
     <div class="mainP">
-        <?php var_dump($_POST);
+    <!-- --> 
+        <?php //var_dump($_POST);
         ?>
         <section class="topBoxnarrows">
             <article class="arrow"><p class="leftArrow arrowP">&lt;</p></article>
             <article class="productBox">
                 <div class="product" id="sigShirt" <?php if ($_POST['val'] != 0) {
                     echo 'style="display:none"';}?>>
-                    <img src="pictures/signature_shirt_black_forTesting.png" alt="SigShirt" class="productImage">
-                    <div class="productDesc">
-                        <p>
-                            This piece features our signature logo, split into two parts. Cut in a regular fit from pure cotton, it features the lettering “no” and “heart” written across both chest halves respectively. This minimal but bold design reflects on the ever-growing divide in modern society.
-                        </p>
-                        <p>
-                            Detailed Information
-                        </p>
-                        <p>
-                        • White, Creme or Black<br>• Chestprint
-                        </p>
+                    <div class="container">
+                        <div class="row prodBoxRow">
+                            <div class="col-3 leftBlock">
+                                
+                                <!-- Front, Back and Close Up pics from shirt-->
+                                <img src="pictures/shirt_front_white_signature.svg" alt="SigShirt" class="productImage white frontPic">
+                                <img src="pictures/shirt_back_white.svg" alt="SigShirt" class="productImage backPic">
+                                <img src="pictures/closeup_white_signature.svg" alt="SigShirt" class="productImage closePic">
+                                
+                                <img src="pictures/shirt_front_black_signature.svg" alt="SigShirt" class="productImage black frontPic">
+                                <img src="pictures/shirt_back_black.svg" alt="SigShirt" class="productImage backPic">
+                                <img src="pictures/closeup_black_signature.svg" alt="SigShirt" class="productImage closePic">
+                                
+                                <img src="pictures/shirt_front_creme_signature.svg" alt="SigShirt" class="productImage creme frontPic">
+                                <img src="pictures/shirt_back_creme.svg" alt="SigShirt" class="productImage backPic">
+                                <img src="pictures/closeup_creme_signature.svg" alt="SigShirt" class="productImage closePic">
+
+                                <!-- Buttons to switch from front, back and closeup--> 
+                                <div class="container content white">
+                                    <div class="row">
+                                        <div class="col-4 frontPicBut white" >
+                                            <img src="pictures/shirt_front_white_signature.svg" class="pics" alt="Signature Shirt Front">
+                                        </div>
+                                        <div class="col-4 backPicBut white">
+                                            <img src="pictures/shirt_back_white.svg" class="pics" alt="Signature Shirt Back">
+                                        </div>
+                                        <div class="col-4 closePicBut white">
+                                            <img src="pictures/closeup_white_signature.svg" class="pics" alt="Signature Shirt Closeup">
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="container content black">
+                                    <div class="row">
+                                        <div class="col-4 frontPicBut black" >
+                                            <img src="pictures/shirt_front_black_signature.svg" class="pics" alt="Signature Shirt Front">
+                                        </div>
+                                        <div class="col-4 backPicBut black">
+                                            <img src="pictures/shirt_back_black.svg" class="pics" alt="Signature Shirt Back">
+                                        </div>
+                                        <div class="col-4 closePicBut black">
+                                            <img src="pictures/closeup_black_signature.svg" class="pics" alt="Signature Shirt Closeup">
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                                <div class="container content creme">
+                                    <div class="row">
+                                        <div class="col-4 frontPicBut creme" >
+                                            <img src="pictures/shirt_front_creme_signature.svg" class="pics" alt="Signature Shirt Front">
+                                        </div>
+                                        <div class="col-4 backPicBut creme">
+                                            <img src="pictures/shirt_back_creme.svg" class="pics" alt="Signature Shirt Back">
+                                        </div>
+                                        <div class="col-4 closePicBut creme">
+                                            <img src="pictures/closeup_creme_signature.svg" class="pics" alt="Signature Shirt Closeup">
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <!-- Radio Buttons -->
+                                <div class="points">
+                                    <label class="control control-radio whiteRad">
+                                            <input type="radio" name="radio" />
+                                        <div class="control_indicator control_indicator_white"></div>
+                                    </label>
+                                    <label class="control control-radio blackRad">
+                                            <input type="radio" name="radio" />
+                                        <div class="control_indicator control_indicator_black"></div>
+                                    </label>
+                                    <label class="control control-radio cremeRad">
+                                            <input type="radio" name="radio" />
+                                        <div class="control_indicator control_indicator_creme"></div>
+                                    </label>
+                                </div>
+                            </div>
+
+                            <!-- Vertical Line which seperates shirts with text-->
+                            <div class="col-1 lineDiv">
+                                <p class="line">.</p>
+                            </div>
+
+                            <!-- Right side, text and detailed information-->
+                            <div class="productDesc col-8">
+                                <h1> Signature Shirt </h1>
+
+                                <p>
+                                    This piece features our signature logo. It features the lettering “no” and “heart” written across the left chest. This design is intentionally minimal, serving as an easy to wear, everyday addition to any wardrobe.
+                                </p>
+                                <p>
+                                    Detailed Information<br> 
+                                    - White, Creme or Black<br>
+                                    - Regular fit<br>
+                                    - Chestprint
+                                </p>                                
+                            </div>
+                        </div>
                     </div>
                 </div>
+
+                
+
                 <div class="product" id="dystShirt" <?php if ($_POST['val'] != 1) {
                     echo 'style="display:none"';}?>>
-                    <img src="pictures/black-shirt-dystopian-back.png" alt="DystShirt" class="productImage">
-                    <div class="productDesc">
-                        <p>
-                            This piece channels the collection’s dystopian theme, with a print of a deserted city in the shape of a heart on the back of the T-shirt. It features the brand logo as a small detail aswell.
-                        </p>
-                        <p>
-                            Detailed Information
-                        </p>
-                        <p>
-                        • White, Creme or Black<br>• Backprint
-                        </p>
+                    <div class="container">
+                        <div class="row prodBoxRow">
+                            <div class="col-3 leftBlock">
+                                <img src="pictures/signature_shirt_white.png" alt="SigShirt" class="productImage white">
+                                <img src="pictures/signature_shirt_black_forTesting.png" alt="SigShirt" class="productImage black">
+                                <img src="pictures/signature_shirt_creme_forTesting.png" alt="SigShirt" class="productImage creme">
+                                
+
+                                <div class="points">
+                                    <label class="control control-radio whiteRad">
+                                            <input type="radio" name="radio" />
+                                        <div class="control_indicator control_indicator_white"></div>
+                                    </label>
+                                    <label class="control control-radio blackRad">
+                                            <input type="radio" name="radio" />
+                                        <div class="control_indicator control_indicator_black"></div>
+                                    </label>
+                                    <label class="control control-radio cremeRad">
+                                            <input type="radio" name="radio" />
+                                        <div class="control_indicator control_indicator_creme"></div>
+                                    </label>
+                                </div>
+                            </div>
+
+                            <!-- Vertical Line which seperates shirts with text-->
+                            <div class="col-1 lineDiv">
+                                <p class="line">.</p>
+                            </div>
+
+                            <!-- Right side, text and detailed information-->
+                            <div class="productDesc col-8">
+                                <h1> Dystopian Shirt </h1>
+                                <p>
+                                    This piece channels the collection’s dystopian theme, with a print of a deserted city in the shape of a broken heart on the back of the T-shirt. It features the brand logo as a small detail on the front.
+                                </p>
+                                <p>
+                                    Detailed Information<br> 
+                                    - White, Creme or Black<br>
+                                    - Regular fit <br>
+                                    - Backprint
+                                </p>                                
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div class="product" id="dystHood" <?php if ($_POST['val'] != 2) {
                     echo 'style="display:none"';}?>>
-                    <img src="" alt="DystHood" class="productImage">
-                    <div class="productDesc">
-                        <p>
-                            This piece channels the collection’s dystopian theme, with a print of a deserted city in the shape of a heart on the back of the T-shirt. It features the brand logo as a small detail aswell.
-                        </p>
-                        <p>
-                            Detailed Information
-                        </p>
-                        <p>
-                        • White, Creme or Black<br>• Backprint
-                        </p>
+                    <div class="container">
+                        <div class="row prodBoxRow">
+                            <div class="col-3 leftBlock">
+                                <img src="pictures/signature_shirt_white.png" alt="SigShirt" class="productImage white">
+                                <img src="pictures/signature_shirt_black_forTesting.png" alt="SigShirt" class="productImage black">
+                                <img src="pictures/signature_shirt_creme_forTesting.png" alt="SigShirt" class="productImage creme">
+                                
+
+                                <div class="points">
+                                    <label class="control control-radio whiteRad">
+                                            <input type="radio" name="radio" />
+                                        <div class="control_indicator control_indicator_white"></div>
+                                    </label>
+                                    <label class="control control-radio blackRad">
+                                            <input type="radio" name="radio" />
+                                        <div class="control_indicator control_indicator_black"></div>
+                                    </label>
+                                    <label class="control control-radio cremeRad">
+                                            <input type="radio" name="radio" />
+                                        <div class="control_indicator control_indicator_creme"></div>
+                                    </label>
+                                </div>
+                            </div>
+
+                            <!-- Vertical Line which seperates shirts with text-->
+                            <div class="col-1 lineDiv">
+                                <p class="line">.</p>
+                            </div>
+
+                            <!-- Right side, text and detailed information-->
+                            <div class="productDesc col-8">
+                                <h1> Dystopian Hoodie </h1>
+                                <p>
+                                    This piece channels the collection’s dystopian theme, with a print of a deserted city in the shape of a broken heart on the back of the hoodie. It features the brand logo as a small detail on the front.
+                                </p>
+                                <p>
+                                    Detailed Information<br> 
+                                    - White, Creme or Black<br>
+                                    - Slightly oversized fit <br>
+                                    - Backprint
+                                </p>                                
+                            </div>
+                        </div>
                     </div>
                 </div>
             </article>
             <article class="arrow"><p class="rightArrow arrowP">&gt;</p></article>
         </section>
     </div>
-    <div class="left">Pfeil</div>
-        <div class="b" id="b1">Inhalt1</div>
-        <div class="b" id="b2">Inhalt2</div>
-        <div class="b" id="b3">Inhalt3</div>
-        <div class="b" id="b4">Inhalt4</div>
-        <div class="right">Pfeil</div>
 </body>
 </html>
